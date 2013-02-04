f22dc945c7fabcb27c490f8f92276d13c17163aa
Yujun Wu <wyj0912@gmail.com>
2013-02-03 19:13:30 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hcop::Application.config.secret_token = 'aa9021ddec27bb089498c05458eb4eb3ae2af33cd0b439c6562df7e4421add8aa7356c759b9689517aa506afff0b1a0f85c32321f0bf98377e279dd0c98837fd'
