245bd5e6c0890cbc07b7c18e35ec3eca35763ce3
Yujun Wu <wyj0912@gmail.com>
2013-02-06 19:18:17 -0800

    add pictures

--- app/models/person.rb
+++ app/models/person.rb
@@ -10,15 +10,15 @@ class Person
 
   def self.nine_students
 	  	students = Array.new
-	  	students[0] = Person.new('Mike','Stanford University','Student profile goes here','Student project goes here',"http://placehold.it/160x160")
-	  	students[1] = Person.new('Jim','Stanford University','Student profile goes here','Student project goes here',"http://placehold.it/160x160")
-	  	students[2] = Person.new('Alan','Stanford University','Student profile goes here','Student project goes here',"http://placehold.it/160x160")
-	  	students[3] = Person.new('John','Stanford University','Student profile goes here','Student project goes here',"http://placehold.it/160x160")
-	  	students[4] = Person.new('Lily','Stanford University','Student profile goes here','Student project goes here',"http://placehold.it/160x160")
-	  	students[5] = Person.new('Emily','Stanford University','Student profile goes here','Student project goes here',"http://placehold.it/160x160")
-	  	students[6] = Person.new('Leo','Stanford University','Student profile goes here','Student project goes here',"http://placehold.it/160x160")
-	  	students[7] = Person.new('James','Stanford University','Student profile goes here','Student project goes here',"http://placehold.it/160x160")
-	  	students[8] = Person.new('Bryan','Stanford University','Student profile goes here','Student project goes here',"http://placehold.it/160x160")
+	  	students[0] = Person.new('Mike','Stanford University','Student profile goes here','Student project goes here',"/images/student_photo_1.jpg")
+	  	students[1] = Person.new('Jim','Stanford University','Student profile goes here','Student project goes here',"/images/student_photo_2.jpg")
+	  	students[2] = Person.new('Alan','Stanford University','Student profile goes here','Student project goes here',"/images/student_photo_3.jpg")
+	  	students[3] = Person.new('John','Stanford University','Student profile goes here','Student project goes here',"/images/student_photo_4.jpg")
+	  	students[4] = Person.new('Lily','Stanford University','Student profile goes here','Student project goes here',"/images/student_photo_5.jpg")
+	  	students[5] = Person.new('Emily','Stanford University','Student profile goes here','Student project goes here',"/images/student_photo_6.jpg")
+	  	students[6] = Person.new('Leo','Stanford University','Student profile goes here','Student project goes here',"/images/student_photo_7.jpg")
+	  	students[7] = Person.new('James','Stanford University','Student profile goes here','Student project goes here',"/images/student_photo_8.jpg")
+	  	students[8] = Person.new('Bryan','Stanford University','Student profile goes here','Student project goes here',"/images/student_photo_9.jpg")
 	    return students
 	end
 end
