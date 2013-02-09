96a5d9ab5c475d9553e8bf25c1053919d905e7ac
Yujun Wu <wyj0912@gmail.com>
2013-02-08 22:39:31 -0800

    animation done

--- app/assets/javascripts/slide.js
+++ app/assets/javascripts/slide.js
@@ -1,29 +1,44 @@
 (function($){
-  $.fn.hoverEffect = function(){
-  	$selector = this;
-    $selector.find('.span4').hover(function(){
-      //$(this).children('.img-cover').fadeOut();
-      $(this).children('.img-cover').addClass('invisible');
-      //send ajax load request
-      var id = $(this).attr('id').replace(/student_/,'');
-      console.log(id);
-      var data = {'id' : id};
-      $('#intro-wrapper').load('/student',data);
-    },function(){
-      //$(this).children('.img-cover').css('display','block');
-      $(this).children('.img-cover').removeClass('invisible');
-    });
+  $.fn.hoverEffect = function(options){
 
-  }
-  $.fn.initialize = function(){
-    //need image loading callback
-    $selector = this;
-    var $img = $selector.find('img:first');
-    $img.load(function(){
-      var height = $img.height();
-      var width = $img.width();
-      $('.img-cover').height(height);
-      $('.img-cover').width(width);
-    }) 
+    var settings = $.extend({
+      hightlight : 1
+    },options);
+
+    $.fn.initialize = function(){
+      //need image loading callback
+      var $img = this.find('img:first');
+      $img.load(function(){
+        var height = $img.height();
+        var width = $img.width();
+        $('.img-cover').height(height);
+        $('.img-cover').width(width);
+        var id = settings.hightlight - 1;
+        $('#student_'+id).find('.img-cover').addClass('invisible');
+        sendAjaxCall(id);
+      }) 
+    } 
+
+  	$selector = this;
+    $selector.initialize();
+    $selector.find('.span4').hover(function(){
+      $imgCover = $(this).children('.img-cover');
+      if($imgCover.css('display') != 'none'){
+        var id = $(this).attr('id').replace(/student_/,'');
+        $('.img-cover').removeClass('invisible');
+        $imgCover.addClass('invisible');
+        //send ajax load request
+        $('#intro-container').show('slide',{direction: 'left'},500,function(){
+          $(this).clearQueue();
+        });
+        sendAjaxCall(id); 
+      } 
+    },function(){});
+
+    function sendAjaxCall(id){
+      var data = {'id' : id};
+      $('#intro-container').load('/student',data);
+    }
+
   }
 })(jQuery)
